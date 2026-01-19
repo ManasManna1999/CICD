3a0f1a5f3d27d7ee2c0f7a1fddc3eea6a31a5ba2
ManasManna1999 <Manas.Manna@ibm.com>
2026-01-19 11:07:14 +0530

    Update main.go
    added comment

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "fmt"
 func Add(a, b int) int {
 	return a + b
 }
-//given a comments to check the pipeline
+//given a comments to check the pipelines
 func main() {
 	fmt.Println("CI/CD Demo: 1 + 2 =", Add(1, 2))
 }
